dcb210a555c97eccf180523b84468b35e50dd4c5
T-Farmer <terrillfarmer@gmail.com>
2017-03-20 09:54:33 -0500

    working on test functions with Jesse

--- bangazonCli.js
+++ bangazonCli.js
@@ -6,65 +6,6 @@ const {Database} = require('sqlite3')
 
 const db = new Database('./data_base/bangazon.sqlite')
 
-//create customers
-
-function createCustomers() {
-    	   db.run(`CREATE TABLE IF NOT EXISTS 'customers' (
-    		 'customerId' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    		 'name' TEXT,
-    		 'address' TEXT,
-    		 'city' TEXT,
-    		 'state' TEXT,
-    		 'postalCode' TEXT,
-    		 'phoneNumber' TEXT
-    		 );`)
-}
-
-//create payment options
-
-function createPaymentOptions() {
-    		 db.run(`CREATE TABLE IF NOT EXISTS 'payment_options' (
-    		 'paymentOptionId' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    		 'paymentName' TEXT,
-    		 'accountNumber' TEXT,
-    		 'customerId' INTEGER,
-    		 FOREIGN KEY('customerId') REFERENCES 'customers'('customerId')
-    		 );`)
-}
-
-//create orders
-
-function createOrders() {
-         db.run(`CREATE TABLE IF NOT EXISTS 'orders' (
-         'orderId' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-         'paymentOptionId' INTEGER NOT NULL,
-         'paidInFull' INTEGER CHECK(paidInFull = 1 or paidInFull = -1),
-         FOREIGN KEY ('paymentOptionId') REFERENCES payment_options(paymentOptionId)
-         );`)
-}
-
-//create line items
-
-function createOrderLineItems() {
-      	 db.run(`CREATE TABLE IF NOT EXISTS 'order_line_items' (
-      	 'lineItemId' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-      	 'orderId' INTEGER NOT NULL,
-      	 'productId' INTEGER NOT NULL,
-      	 FOREIGN KEY ('orderId') REFERENCES orders(orderId),
-      	 FOREIGN KEY ('productId') REFERENCES products(productId)
-      	 );`)
-}
-
-//create products
-
-function createProducts() {
-      	 db.run(`CREATE TABLE IF NOT EXISTS 'products' (
-      	 'productId' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-      	 'productName' INTEGER,
-      	 'productPrice' INTEGER
-      	 );`)
-}
-
 function createAll() {
 	       createCustomers()
          createPaymentOptions()
