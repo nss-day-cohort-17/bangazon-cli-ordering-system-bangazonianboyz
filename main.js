1d08109025028e11866a41179171198ce7ef9cd9
T-Farmer <terrillfarmer@gmail.com>
2017-03-21 11:02:58 -0500

    resolving password issue

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-'use strict'
-
-//get user prompts
-const prompt = require('prompt')
-// const { changePage } = require('./changePage.js')
-
-module.exports.displayMainMenu = () => {
- //   console.log(` *********************************************************
- // **  Welcome to Bangazon! Command Line Ordering System  **
- // *********************************************************`)
- //  	console.log('1. Create a customer account')
- //  	console.log('2. Choose active customer')
- //  	console.log('3. Create a payment option')
- //  	console.log('4. Add product to shopping cart')
- //  	console.log('5. Complete an order')
- //  	console.log('6. See product popularity')
- //  	console.log('7. Leave Bangazon!')
-
-	module.exports.mainMenu();
-}
-
-module.exports.mainMenu = () => {}
+// 'use strict'
+//
+// //get user prompts
+// const prompt = require('prompt')
+// // const { changePage } = require('./changePage.js')
+//
+// module.exports.displayMainMenu = () => {
+//
+//
+// 	module.exports.mainMenu();
+// }
+//
+// module.exports.mainMenu = () => {}
