5d53975b6bef08e2d4b6455cd2c409714002de41
T-Farmer <terrillfarmer@gmail.com>
2017-03-21 10:34:43 -0500

    deleted old code

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const prompt = require('prompt')
 // const { changePage } = require('./changePage.js')
 
 module.exports.displayMainMenu = () => {
-	  console.log(` *********************************************************
- **  Welcome to Bangazon! Command Line Ordering System  **
- *********************************************************`)
-  	console.log('1. Create a customer account')
-  	console.log('2. Choose active customer')
-  	console.log('3. Create a payment option')
-  	console.log('4. Add product to shopping cart')
-  	console.log('5. Complete an order')
-  	console.log('6. See product popularity')
-  	console.log('7. Leave Bangazon!')
+ //   console.log(` *********************************************************
+ // **  Welcome to Bangazon! Command Line Ordering System  **
+ // *********************************************************`)
+ //  	console.log('1. Create a customer account')
+ //  	console.log('2. Choose active customer')
+ //  	console.log('3. Create a payment option')
+ //  	console.log('4. Add product to shopping cart')
+ //  	console.log('5. Complete an order')
+ //  	console.log('6. See product popularity')
+ //  	console.log('7. Leave Bangazon!')
 
 	module.exports.mainMenu();
 }
